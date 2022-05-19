07c8117bc0e7332556fb2d882df5a7530dcc2827
fenil patel <fenilpatel12997@gmail.com>
2022-05-19 10:01:18 +0530

    learned switch branch without commit

--- fun01.py
+++ fun01.py
@@ -1,2 +1,3 @@
 print("fun 1 added into master branch and learn add,commit in one cmd")
 print("learning about checkout discard change functionality")
+print("try to switch branch without commited into current branch")
