776715383c0a51c0d398f04c42c9e99475bff6e4
fenil patel <fenilpatel12997@gmail.com>
2022-05-21 18:45:16 +0530

    new line conflict added

--- fun01.py
+++ fun01.py
@@ -1,7 +1,7 @@
 print("fun 1 added into master branch and learn add,commit in one cmd")
 print("learning about checkout discard change functionality")
-print("try to move branch without commited into current branch")
+print("try to switch branch without commited into current branch")
 
 print("fast forward branch added")  
-print("learning only fast forward")
+print("new line added")
 
