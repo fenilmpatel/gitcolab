0159cf8b0cd28db968dc55b7f4518571ba4f42c8
fenil patel <fenilpatel12997@gmail.com>
2022-05-21 17:33:05 +0530

    fast forward branch added

--- fun01.py
+++ fun01.py
@@ -1,3 +1,5 @@
 print("fun 1 added into master branch and learn add,commit in one cmd")
 print("learning about checkout discard change functionality")
 print("try to switch branch without commited into current branch")
+
+print("fast forward branch added")
