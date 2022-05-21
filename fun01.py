db1a130c6bdb6374a5cff2dd34946f3d4865f7bc
fenil patel <fenilpatel12997@gmail.com>
2022-05-21 18:43:03 +0530

    confilct change into master

--- fun01.py
+++ fun01.py
@@ -3,5 +3,5 @@ print("learning about checkout discard change functionality")
 print("try to move branch without commited into current branch")
 
 print("fast forward branch added")  
-print("learning only fast forward")
+print("learning only fast forward and confilt happened into master")
 
